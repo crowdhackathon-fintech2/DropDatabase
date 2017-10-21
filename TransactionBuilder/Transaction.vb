6851ba940d5b70187e0fbb36cf8aad8a6543f9ad
OrestisAlpos <or.al@hotmail.com>
2017-10-21 15:41:38 +0300

    Created TransactionBuilder

---
+++ TransactionBuilder/Transaction.vb
@@ -0,0 +1,100 @@
+﻿Public Class Holder
+    Public Property name As String
+    Public Property is_alias As Boolean
+End Class
+
+Public Class Bank
+    Public Property name As String
+    Public Property national_identifier As String
+End Class
+
+Public Class ThisAccount
+    Public Property id As String
+    Public Property holders As Holder()
+    Public Property number As String
+    Public Property kind As String
+    Public Property IBAN As String
+    Public Property swift_bic As String
+    Public Property bank As Bank
+End Class
+
+Public Class OtherAccount
+    Public Property id As String
+    Public Property holders As Holder()
+    Public Property number As String
+    Public Property kind As String
+    Public Property IBAN As String
+    Public Property swift_bic As String
+    Public Property bank As Bank
+End Class
+
+Public Class NewBalance
+    Public Property currency As String
+    Public Property amount As String
+End Class
+
+Public Class Value
+    Public Property currency As String
+    Public Property amount As String
+End Class
+
+Public Class Details
+    Public Property type As String
+    Public Property description As String
+    Public Property posted As DateTime
+    Public Property completed As DateTime
+    Public Property new_balance As NewBalance
+    Public Property value As Value
+End Class
+
+Public Class User
+    Public Property id As String
+    Public Property provider As String
+    Public Property display_name As String
+End Class
+
+Public Class Comment
+    Public Property id As String
+    Public Property value As String
+    Public Property execdate As DateTime
+    Public Property user As User
+End Class
+
+Public Class Tag
+    Public Property id As String
+    Public Property value As String
+    Public Property execdate As DateTime
+    Public Property user As User
+End Class
+
+Public Class Image
+    Public Property id As String
+    Public Property label As String
+    Public Property URL As String
+    Public Property execdate As DateTime
+    Public Property user As User
+End Class
+
+Public Class Where
+    Public Property latitude As Double
+    Public Property longitude As Double
+    Public Property execdate As DateTime
+    Public Property user As User
+End Class
+
+Public Class Metadata
+    Public Property narrative As String
+    Public Property comments As Comment()
+    Public Property tags As Tag()
+    Public Property images As Image()
+    Public Property where As Where
+End Class
+
+Public Class Transaction
+    Public Property id As String
+    Public Property this_account As ThisAccount
+    Public Property other_account As OtherAccount
+    Public Property details As Details
+    Public Property metadata As Metadata
+End Class
+
--- TransactionBuilder/TransactionBuilder.vb
+++ TransactionBuilder/TransactionBuilder.vb
@@ -1,15 +1,44 @@
-﻿Public Class TransactionBuilder
-
-End Class
-Public Class SandboxBuilder
+﻿Imports newtonsoft.json
 
 
+Module Module1
 
-    ' executionDate in form: '2017-10-21'
-    Private Sub AppendTransaction(description As String, executionDate As String, amount As Decimal)
+    Sub Main()
 
+        Dim SandboxBuilder As New SandboxBuilder
+        SandboxBuilder.CreateTransactionList()
 
     End Sub
+
+End Module
+
+
+Public Class SandboxBuilder
+    Dim InitialSandbox As String = My.Computer.FileSystem.ReadAllText("..\..\sandbox.json")
+    Dim TransactionTemplate As String = My.Computer.FileSystem.ReadAllText("..\..\transactionTemplate.json")
+
+
+    Public Function CreateTransactionList() As Transaction()
+        'Dim TransactionList As Transaction()
+        Dim Transaction As Transaction
+        Dim Transactions As New List(Of Transaction)
+
+        Transaction = CreateTransaction("XXX", "2017-10-21", 9999)
+        Transactions.Add(Transaction)
+
+        Return Nothing
+    End Function
+
+    ' executionDate in form: '2017-10-21'
+    Public Function CreateTransaction(description As String, executionDate As String, amount As Decimal) As Transaction
+        Dim Transaction = JsonConvert.DeserializeObject(Of Transaction)(TransactionTemplate)
+        Transaction.id = New Guid().ToString
+        Transaction.details.description = description
+        Transaction.details.value.amount = amount
+        Transaction.details.posted = CType(executionDate + "T10:08:54.753Z", DateTime)
+        Transaction.details.completed = CType(executionDate + "T10:08:54.753Z", DateTime)
+        Return Transaction
+    End Function
 
 
 End Class
