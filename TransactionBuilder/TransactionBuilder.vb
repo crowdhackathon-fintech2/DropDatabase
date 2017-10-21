4a058c2ea55c6adbe4eb7af4ba093fa5a35fbb08
OrestisAlpos <or.al@hotmail.com>
2017-10-21 13:59:11 +0300

    Initialized project for creating Transactions in the sandbox.

---
+++ TransactionBuilder/TransactionBuilder.vb
@@ -0,0 +1,15 @@
+﻿Public Class TransactionBuilder
+
+End Class
+Public Class SandboxBuilder
+
+
+
+    ' executionDate in form: '2017-10-21'
+    Private Sub AppendTransaction(description As String, executionDate As String, amount As Decimal)
+
+
+    End Sub
+
+
+End Class
