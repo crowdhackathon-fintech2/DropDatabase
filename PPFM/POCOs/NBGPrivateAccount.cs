581e3ceaaa44ca916423f0bd7ca919383c3a7500
George <georgiosdroubalis@hotmail.gr>
2017-10-21 13:28:05 +0300

    update nbgprivateaccount cs

--- PPFM/POCOs/NBGPrivateAccount.cs
+++ PPFM/POCOs/NBGPrivateAccount.cs
@@ -9,10 +9,24 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
 
 namespace PPFM.POCOs
 {
-    class NBGPrivateAccount
+    public class NBGPrivateAccount
     {
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("label")]
+        public string label { get; set; }
+
+        [JsonProperty("bank_id")]
+        public string bank_id { get; set; }
+
+        [JsonProperty("views_available")]
+        public IList<ViewsAvailable> views_available { get; set; }
     }
+
 }
