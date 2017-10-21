f24ffd8e88db163551f5ad5ea0819307296d83c5
George <georgiosdroubalis@hotmail.gr>
2017-10-21 13:20:08 +0300

    Added POCOs

---
+++ PPFM/POCOs/NBGAccounts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace PPFM.POCOs
+{
+    public class NBGAccounts
+    {
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("label")]
+        public string label { get; set; }
+
+        [JsonProperty("bank_id")]
+        public string bank_id { get; set; }
+
+        [JsonProperty("views_available")]
+        public IList<ViewsAvailable> views_available { get; set; }
+    }
+}
---
+++ PPFM/POCOs/ViewsAvailable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace PPFM.POCOs
+{
+    public class ViewsAvailable
+    {
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("short_name")]
+        public string short_name { get; set; }
+
+        [JsonProperty("is_public")]
+        public bool is_public { get; set; }
+    }
+}
